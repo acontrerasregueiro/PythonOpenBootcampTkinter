aa6b544e998a0cbf0f1d1adeb46cfb7c9814ade6
Adrian <acontrerasregueiro@gmail.com>
2022-09-08 08:00:08 +0200

    Iniciacion a tkinter, geometria Pack

---
+++ main.py
@@ -0,0 +1,24 @@
+#Iniciacion con tkinter crear una ventana
+#Importamos los componentes necesarios
+from ctypes import alignment
+import tkinter
+
+#Geometria Pack, cuando queremos posicionar elementos de arriba a abajo o cara a cara (izq-der)
+window = tkinter.Tk() #Creamos una instancia de tk
+
+
+label1 = tkinter.Label(window,text= "Label1", bg='yellow', fg='blue') #creamos un label
+label1.pack(ipadx=45,ipady=15 ,fill='x', side='left') 
+label2 = tkinter.Label(window,text= "Label2", bg='orange', fg='green') #creamos un label
+label2.pack(ipadx=45,ipady=15 ,fill='x', side='left') 
+label3 = tkinter.Label(window,text= "Label3", bg='blue', fg='red') #creamos un label
+label3.pack(ipadx=45,ipady=15, side='left') 
+label4 = tkinter.Label(window,text= "Label4", bg='yellow', fg='black') #creamos un label
+label4.pack(ipadx=45,ipady=15 ,fill='x', side='left') 
+
+label_adios = tkinter.Label(window,text= 'adios', bg='red', fg='white')
+label_adios.pack(ipadx=50,ipady=50,side='left')
+
+
+
+window.mainloop()# Bucle necesario
